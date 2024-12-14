c9ee0c0fe9d10c2e0bb6ca22fede855a33bdf6b8
HaiGH <nthai0932169055@gmail.com>
2024-12-14 10:54:41 +0700

    update file

---
+++ index.js
@@ -0,0 +1,23 @@
+import {api} from "./api/api.js";
+import {readFileSync} from 'fs'
+import {nhom_to, assignElem, mon_hoc} from "./api/model.js";
+
+async function init() {
+    const userJSON = JSON.parse(readFileSync('./user.json', 'utf8'));
+    await api.login(userJSON.username,userJSON.password)
+    // await api.dsDieuKienloc()
+
+    // await api.dsKQdkmh()
+    // const dsThongTin =  await api.dsThongTin()
+    // dsThongTin.ds_nhom_to.forEach(value =>{
+    //     assignElem(nhom_to,value)
+    //
+    //     if (nhom_to.id_to_hoc === '-8263160012138159500') {
+    //         console.log(value)
+    //     }
+    // })
+    // await api.dkmh("-8263160012138159500",true)
+    // await api.dkmh("-8263160012138159500",false)
+}
+
+init();
