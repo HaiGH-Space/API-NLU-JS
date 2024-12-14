b8322a75a869fe5bca138bb5d78337763fbaf27d
HaiGH <nthai0932169055@gmail.com>
2024-12-14 19:56:26 +0700

    add model dkmh and more

--- api/model.js
+++ api/model.js
@@ -1,4 +1,4 @@
-let sinh_vien = {
+export let sinh_vien = {
     mail: "",
     name: "",
     refresh_token: "",
@@ -7,25 +7,77 @@ let sinh_vien = {
     token: "",
     access_token: ""
 }
+export let dkmh = {
+    is_thanh_cong: false,
+    thong_bao_loi: '',
+    is_chung_nhom_mon_hoc: false,
+    ket_qua_dang_ky: {
+        id_kqdk: '',
+        ngay_dang_ky: '',
+        is_da_rut_mon_hoc: false,
+        enable_xoa: false,
+    }
+}
+export let diemSinhVien = {
+    ds_diem_hocky: [],
+    mesage_diemk1:'',
+    mesage_diemtk4:'',
+    mesage_diemtk10:'',
+    mesage_diemtkc:''
+}
+export let diem_hocky = {
+    ds_diem_mon_hoc : [],
+    loai_nganh: -1,
+    hoc_ky: "",
+    ten_hoc_ky: "",
+    dtb_hk_he10: "",
+    dtb_hk_he4: "",
+    dtb_tich_luy_he_10: "",
+    dtb_tich_luy_he_4: "",
+    so_tin_chi_dat_hk: "",
+    so_tin_chi_dat_tich_luy: "",
+    xep_loai_tkb_hk: "",
+}
+export let diem_mon_hoc = {
+    ma_mon: "",
+    nhom_to: "",
+    ten_mon: "",
+    ten_mon_eg: "",
+    mon_hoc_nganh: true,
+    so_tin_chi: "0",
+    diem_thi: "0",
+    diem_giua_ky: "0",
+    diem_tk: "0",
+    diem_tk_so: "0",
+    diem_tk_chu: "",
+    loai_nganh: -1,
+    ds_diem_thanh_phan: []
 
-let mon_hoc = {
+}
+export let diem_thanh_phan = {
+    ky_hieu: "",
+    ten_thanh_phan: "",
+    trong_so: "",
+    diem_thanh_phan: ""
+}
+export let mon_hoc = {
     ma: '',
     ten: ''
 }
-let khoa = {
+export let khoa = {
     ma: '',
     ten: ''
 }
-let lop = {
+export let lop = {
     ma: '',
     ten: ''
 }
-let dk_loc = {
+export let dk_loc = {
     "gia_tri": '',
     "mieu_ta": '',
     "is_mac_dinh": ''
 }
-let nhom_to = {
+export let nhom_to = {
     id_mon: '',
     id_to_hoc: '',
     ma_mon: '',
@@ -45,7 +97,7 @@ let nhom_to = {
     is_ctdt: false
 
 }
-let kq_dk = {
+export let kq_dk = {
     enable_xoa : false,
     hoc_phi_tam_tinh: 0,
     id_dia_diem_thi: '',
@@ -64,11 +116,11 @@ let kq_dk = {
         so_tc: ''
     }
 }
-function assignElem(element,data) {
+export function assignElem(element,data) {
     Object.assign(element, data);
 }
-let ds_dk_loc = []
-let dsThongTin = {
+export let ds_dk_loc = []
+export let dsThongTin = {
     total_items: 0,
     trong_thoi_gian_dang_ky: false,
     ds_khoa: [],
@@ -76,12 +128,11 @@ let dsThongTin = {
     ds_mon_hoc:[],
     ds_nhom_to: []
 }
-let ket_qua_dkmh = {
+export let ket_qua_dkmh = {
     is_chung_nhom_mon_hoc: false,
     is_thanh_cong: false,
     ket_qua_dang_ky: kq_dk,
     thong_bao_loi: '',
     thong_bao_tien_quyet: ''
 }
-let ds_kq_dk = []
-export {kq_dk,ds_kq_dk,sinh_vien,nhom_to,dk_loc,ds_dk_loc,dsThongTin,khoa,lop,mon_hoc,assignElem}
+export let ds_kq_dk = []
